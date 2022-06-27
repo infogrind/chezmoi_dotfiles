504a97ad722dc4e62badc6d510cb7f1d189ef3b9
Marius Kleiner <kleiner@gmail.com>
2022-06-27 17:35:04 +0200

    [nvim] Installed vim-plug
    I installed it not in the default ~/.local/share/nvim/site/autoload
    directory but under ~/.config/nvim/autoload instead, so that I can don't
    have to add anything in ~/.local/share/nvim to version control.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -6,6 +6,10 @@
 -- already sets many reasonable default options. See `help :nvim-defaults` for
 -- details.
 
+-- Emulate vim-plug's Plug command, I'll use it in the plugin section below.
+-- See https://dev.to/vonheikemen/neovim-using-vim-plug-in-lua-3oom.
+local Plug = vim.fn['plug#']
+
 -- Set a default text width
 vim.opt.textwidth = 80
 
@@ -15,3 +19,14 @@ vim.opt.mouse = 'a'
 
 -- Set wildcard mode to bash-like and show options in menu
 vim.opt.wildmode = 'longest,full'
+
+-- Plugin section
+-- See 
+
+
+vim.call('plug#begin')
+
+-- Install plugins here, e.g.:
+-- Plug 'tpope/vim-sensible'
+
+vim.call('plug#end')
